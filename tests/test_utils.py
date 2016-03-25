156e9d48deeb0cf54ff4f8106d75106bff52df4f
MarcoQin <qyyfy2009@163.com>
2016-03-25 10:42:47 +0800

    update testcase

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import unittest
+
+import _env  # noqa
 import water.utils.common_utils as util
 
 
-def test_Dict():
-    d = util.Dict()
-    d.key = 'value'
-    print d
-    assert d['key'] == d.key
+class TestUtilMethods(unittest.TestCase):
+
+    def test_Dict(self):
+        d = util.Dict()
+        d.key = 'value'
+        d['key1'] = 'value1'
+        self.assertEqual(d['key'], d.key)
+        self.assertEqual(d.key1, d['key1'])
+        self.assertEqual(d.undifined, None)
 
 if __name__ == "__main__":
-    test_Dict()
+    unittest.main()
