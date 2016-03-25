4adf6824a78c3cc5b826ed3a40c84efa915da99d
MarcoQin <qyyfy2009@163.com>
2016-03-25 11:22:48 +0800

    add testcase

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import json
 import unittest
+from datetime import datetime
 
 import _env  # noqa
 import water.utils.common_utils as util
@@ -16,6 +18,24 @@ class TestUtilMethods(unittest.TestCase):
         self.assertEqual(d['key'], d.key)
         self.assertEqual(d.key1, d['key1'])
         self.assertEqual(d.undifined, None)
+
+    def test_classproperty(self):
+        class A(object):
+            @util.classproperty
+            def name(cls):
+                return 'classproperty'
+
+        self.assertEqual(A.name, 'classproperty')
+
+    def test_json_encoder(self):
+        now = datetime.now()
+        d = {
+            'time': now
+        }
+        d1 = {
+            'time': now.strftime('%Y-%m-%d %H:%M:%S')
+        }
+        self.assertEqual(json.dumps(d, cls=util.JsonEncoder), json.dumps(d1))
 
 if __name__ == "__main__":
     unittest.main()
