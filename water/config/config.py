218853cf77cfeba9f5e917198992e0568991e806
MarcoQin <qyyfy2009@163.com>
2016-03-27 20:37:02 +0800

    config

--- water/config/config.py
+++ water/config/config.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-MYSQL_HOST = '127.0.0.1'
-MYSQL_PORT = '3306'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = '1234'
-MYSQL_DATABASE = 'test'
+import os
+import ConfigParser
+
+from utils.common_utils import Dict
+
+
+MYSQL_CONFIG = Dict(
+    host='127.0.0.1',
+    port='3306',
+    user='root',
+    password='1234',
+    database='test'
+)
+
+LOG_DIR = '/home/marcoqin/server_logs'
+
+logging_config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logging.cfg')
+LOG_CONFIG = ConfigParser.ConfigParser(None)
+LOG_CONFIG.read(logging_config_file_path)
+
+MAX_WAIT_SECONDS_BEFORE_SHUTDOWN = 5
