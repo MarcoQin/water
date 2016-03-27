0dd489c84bc6a6bdd1c0f3551189fc25967cadb7
MarcoQin <qyyfy2009@163.com>
2016-03-27 20:37:54 +0800

    and logger and handle signal

--- water/server.py
+++ water/server.py
@@ -2,6 +2,9 @@
 # encoding: utf-8
 
 import os
+import time
+import logging
+import signal
 
 import tornado.ioloop
 import tornado.web
@@ -9,9 +12,33 @@ import tornado.web
 from tornado import httpserver
 from tornado.options import define, options
 
+from inits.log_init import init_log
+from config.config import MAX_WAIT_SECONDS_BEFORE_SHUTDOWN
+
 
 define('port', default=9000, help="run on the given port", type=int)
 define('debug', default=True, help="debug mode", type=bool)
+
+
+def close_server():
+    deadline = time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN
+
+    def stop_loop():
+        now = time.time()
+        io_loop = tornado.ioloop.IOLoop.instance()
+        if now < deadline and (io_loop._callbacks or io_loop._timeouts):
+            io_loop.add_timeout(now + 1, stop_loop)
+        else:
+            io_loop.stop()
+            logging.info('...Shutdown...')
+    stop_loop()
+    logging.info("...close_httpserver():ready...")
+
+
+# handle signal
+def server_shutdown_handler(sig, frame):
+    logging.warning('...Caught signal: %s', sig)
+    tornado.ioloop.IOLoop.instance().add_callback(close_server)
 
 
 class App(tornado.web.Application):
@@ -30,12 +57,19 @@ class App(tornado.web.Application):
 def main():
     # handle the commmand line parameters
     tornado.options.parse_command_line()
+
     port = options.port
+    init_log(port)
+
+    # added signal callback to interrupt app
+    signal.signal(signal.SIGINT, server_shutdown_handler)
+    signal.signal(signal.SIGTERM, server_shutdown_handler)
+
     _http_server_app = App()
     http_server = httpserver.HTTPServer(_http_server_app)
     http_server.listen(port)
-    print "Server start on port: ", port
-    print "Address: http://127.0.0.1:%s" % port
+    logging.info('...Server started on port: %s...'% port)
+    logging.info('...Address: http://127.0.0.1:%s ...'% port)
     tornado.ioloop.IOLoop.instance().start()
 
 
