08b0e2cc70c6ed5af6043776e54487e0af25bbc9
MarcoQin <qyyfy2009@163.com>
2016-03-28 18:34:22 +0800

    base handler

--- water/handler/base_handler.py
+++ water/handler/base_handler.py
@@ -10,6 +10,18 @@ class MainHandler(RequestHandler):
     def __init__(self, *request, **kwargs):
         super(MainHandler, self).__init__(*request, **kwargs)
 
+    def prepare(self):
+        """
+        Called at the beginning of a request before 'get' / 'post'/ etc.
+        Here to process prepare extensions(or something like middleware).
+        """
+        pass
+
+    def on_finish(self):
+        """Called after the end of a request.
+        Here to process clean extensions(or some other middleware).
+        """
+
     def get(self):
         print self.request.path
         self.write("hello world")
