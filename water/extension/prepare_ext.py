825921dbe190dbcfdfc10028a47acbc336b69311
MarcoQin <qyyfy2009@163.com>
2016-03-29 12:18:07 +0800

    prepare extension

--- water/extension/prepare_ext.py
+++ water/extension/prepare_ext.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from base_extension import PrepareExt
+from base_extension import PrepareExt, FinishExt
 from route.base_route import ALL_ROUTES
 
 
 class FindView(PrepareExt):
+    """
+    Find processor view via current frame's url path
+    """
 
     def __call__(self):
         path = self.handler.request.path
@@ -13,3 +16,26 @@ class FindView(PrepareExt):
             if _regex.match(path):
                 self.handler.view = _view
                 break
+
+
+class EvalHandlerView(PrepareExt):
+    """
+    Find out current request handler's custom extensions and eval
+    """
+
+    def __call__(self, prepare=True):
+        if not self.handler.view:
+            return
+        exts = self.handler.view.extensions
+        if exts:
+            base = PrepareExt if prepare else FinishExt
+            _be_called_exts = []
+            if isinstance(exts, (tuple, list)):
+                for ext in exts:
+                    if issubclass(ext, base):
+                        _be_called_exts.append(ext)
+            else:
+                if issubclass(exts, base):
+                    _be_called_exts.append(exts)
+            for ext in _be_called_exts:
+                ext(self.handler)()
