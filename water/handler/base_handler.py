dc0ba4d852c54ca9c56f4555482b4ff2f3260702
MarcoQin <qyyfy2009@163.com>
2016-03-29 16:20:39 +0800

    croutin

--- water/handler/base_handler.py
+++ water/handler/base_handler.py
@@ -5,6 +5,7 @@ from tornado.web import RequestHandler, asynchronous
 from tornado import gen
 
 from extension.prepare_ext import FindView, EvalHandlerView
+from utils.exception_utils import NormalException
 
 
 class MainHandler(RequestHandler):
@@ -12,6 +13,10 @@ class MainHandler(RequestHandler):
     def __init__(self, *request, **kwargs):
         super(MainHandler, self).__init__(*request, **kwargs)
         self.view = None
+        self.path_args = ()
+        self.path_kwargs = {}
+        self.tracker = self.application.tracker
+        self.sys_logger = self.application.sys_logger
 
     def prepare(self):
         """
@@ -19,23 +24,32 @@ class MainHandler(RequestHandler):
         Here to process prepare extensions(or something like middleware).
         """
         FindView(self)()  # find process view via current visited url path
-        EvalHandlerView(self)()  # find current view's extensions and evaluate
+        EvalHandlerView(self)(prepare=True)  # find current view's extensions and evaluate
 
     def on_finish(self):
         """Called after the end of a request.
         Here to process clean up extensions(or some other middleware).
         """
-        pass
+        EvalHandlerView(self)(prepare=False)  # find current view's extensions and evaluate
 
-    def get(self):
+    @asynchronous
+    @gen.coroutine
+    def get(self, *args, **kwargs):
         if self.view:
-            res = self.view(self).get()
-            self.write(res)
-            self.finish()
+            try:
+                data = yield self.view(self).get(*self.path_args, **self.path_kwargs)
+                self.write(data)
+                self.finish()
+            except NormalException as e:
+                print e.message
+                pass
+            except Exception as e:
+                print e
+                self.tracker.trace_error()
         self.send_error(404)
         self.finish()
 
     @asynchronous
     @gen.coroutine
-    def post(self):
+    def post(self, *args, **kwargs):
         pass
