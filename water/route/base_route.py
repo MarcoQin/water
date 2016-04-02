b54bdbba26e97bcad068c712400d5ffd22869d2a
MarcoQin <qyyfy2009@163.com>
2016-04-02 16:44:43 +0800

    add another handler jmethod

--- water/route/base_route.py
+++ water/route/base_route.py
@@ -4,12 +4,22 @@
 import re
 
 ALL_ROUTES = []
+API_HANDLER_MAP = {}
 
 
 def route(url):
     if not url.endswith('$'):
         url += '$'
+
     def _view(cls):
         ALL_ROUTES.append((re.compile(url), cls))
+        return cls
+    return _view
+
+
+def api_route(api_id):
+
+    def _view(cls):
+        API_HANDLER_MAP[int(api_id)] = cls
         return cls
     return _view
