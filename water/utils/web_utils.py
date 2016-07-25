7c3ba081700ac231226e152915f23a287a485ea6
MarcoQin <qyyfy2009@163.com>
2016-07-25 17:56:29 +0800

    add web_utils

--- water/utils/web_utils.py
+++ water/utils/web_utils.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import requests
+from urlparse import urlparse
+from gevent import Timeout
+
 
 class Redirect(object):
 
@@ -9,3 +13,24 @@ class Redirect(object):
 
     def do_redirect(self, handler):
         handler.redirect(self.url)
+
+
+class WebUtils(object):
+
+    @classmethod
+    def get_domain(cls, url):
+        return urlparse(url).netloc
+
+    @classmethod
+    def get_redirect_location(cls, url):
+        location = url
+        try:
+            with Timeout(120):
+                r = requests.head(url, allow_redirects=True, timeout=120, verify=False)
+                try:
+                    location = r.url
+                except Exception:
+                    location = r.headers.get('location', url)
+        except Exception:
+            pass
+        return location
