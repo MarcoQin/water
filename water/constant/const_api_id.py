4d4261daf073a6f253522152d0ab444ecbd135d2
MarcoQin <qyyfy2009@163.com>
2016-07-25 18:21:12 +0800

    fix bug

--- water/constant/const_api_id.py
+++ water/constant/const_api_id.py
@@ -6,9 +6,4 @@ from utils.common_utils import Dict
 
 API_ID = Dict(
     HELLO_WORLD_API=10001,  # Test api hello world
-    CALENDAR=Dict(
-        LIST=10002,  # Canlendar event
-        INSERT=10003,  # create event
-        UPDATE=10004,  # create event
-    )
 )
